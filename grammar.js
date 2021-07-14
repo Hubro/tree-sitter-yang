7efdc71e9310bfe00d123438aea78f55f4babeff
Tomas Sandven <tomas@sandven.email>
2021-07-14 04:47:51 +0200

    Completely change strategy!
    Rather than trying to follow the ABNF syntax accurately, simply look at the
    language in terms of statements and blocks instead. This massively simplifies
    the implementation.

--- grammar.js
+++ grammar.js
@@ -4,43 +4,35 @@ module.exports = grammar({
     word: $ => $.identifier,
 
     rules: {
-        /*
-         * Modules
-         */
-
+        // The root node
         module: $ => seq(
-            'module',
+            choice('module', 'submodule'),
             field('module_name', $.identifier),
-            field('module_block', $.module_block),
+            field('module_block', $.block),
         ),
 
-        module_block: $ => seq(
+        block: $ => seq(
             '{',
-            repeat(
-                choice(
-                    $.yang_version_statement,
-                    $.namespace_statement,
-                    $.prefix_statement,
-                ),
-            ),
+            repeat($.statement),
             '}',
         ),
 
-        yang_version_statement: $ => seq(
-            "yang-version", $.string, $._stmsep
+        statement: $ => seq(
+            $.statement_keyword,
+            $._sep,
+            $.value,
+            choice(
+                $._stmtend,
+                $.block,
+            )
         ),
 
-        namespace_statement: $ => seq(
-            "namespace", $.string, $._stmsep
+        value: $ => choice(
+            $.identifier,
+            $.string,
+            $.date,
+            $.keypath,
         ),
-
-        prefix_statement: $ => seq(
-            "prefix", $.identifier, $._stmsep
-        ),
-
-        /*
-         * Generic
-         */
 
         identifier: $ => /[a-zA-Z_][a-zA-Z0-9-_.]*/,
 
@@ -50,10 +42,97 @@ module.exports = grammar({
             '"',
         ),
 
+        date: $ => /\d{4}-\d{2}-\d{2}/,
+
+        keypath: $ => choice(
+            $._absolute_keypath,
+            $._relative_keypath,
+        ),
+
+        _relative_keypath: $ => seq($.node_identifier, $._absolute_keypath),
+        _absolute_keypath: $ => repeat1(seq('/', $.node_identifier)),
+
+        node_identifier: $ => seq(
+            optional(seq($.identifier, token.immediate(":"))),
+            $.identifier
+        ),
+
         /*
          * Misc
          */
 
-        _stmsep: $ => ';',   // Statement separator
+        _sep: $ => /\s+/,      // Separator
+        _stmtend: $ => ';',    // End of statement
+
+        statement_keyword: $ => choice(
+            'action',
+            'anydata',
+            'anyxml',
+            'argument',
+            'augment',
+            'base',
+            'belongs-to',
+            'bit',
+            'case',
+            'choice',
+            'config',
+            'contact',
+            'container',
+            'default',
+            'description',
+            'deviate',
+            'deviation',
+            'enum',
+            'error-app-tag',
+            'error-message',
+            'extension',
+            'feature',
+            'fraction-digits',
+            'grouping',
+            'identity',
+            'if-feature',
+            'import',
+            'include',
+            'input',
+            'key',
+            'leaf',
+            'leaf-list',
+            'length',
+            'list',
+            'mandatory',
+            'max-elements',
+            'min-elements',
+            'modifier',
+            'module',
+            'must',
+            'namespace',
+            'notification',
+            'ordered-by',
+            'organization',
+            'output',
+            'path',
+            'pattern',
+            'position',
+            'prefix',
+            'presence',
+            'range',
+            'reference',
+            'refine',
+            'require-instance',
+            'revision',
+            'revision-date',
+            'rpc',
+            'status',
+            'submodule',
+            'type',
+            'typedef',
+            'unique',
+            'units',
+            'uses',
+            'value',
+            'when',
+            'yang-version',
+            'yin-element',
+        )
     },
 })
